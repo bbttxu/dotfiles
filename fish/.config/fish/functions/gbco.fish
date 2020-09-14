6e4a6233edd0201a192938299116a0c7a49f9b63
Adam Krawiec <adamkrawiec@gmail.com>
2020-09-14 19:21:46 +0000

    improve gbco fish/function

--- fish/.config/fish/functions/gbco.fish
+++ fish/.config/fish/functions/gbco.fish
@@ -1,7 +1,11 @@
+# TODO strip '* ' and '  ' from git branch listings
+# affects when the current branch is selected and prepended with '* '
 function gbco --description 'fzf powered git branch && checkout'
     if test -n "$argv"
-        git branch | fzf -q $argv | xargs git checkout
+        set branch (git branch | fzf -q $argv) 
     else
-        git branch | fzf | xargs git checkout
+        set branch (git branch | fzf) 
     end
+
+    git checkout (string replace -r -a '(\* {1}| {2})' '' $branch)
 end
